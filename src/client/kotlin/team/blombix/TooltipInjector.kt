f81cfa2dd05fd0ec515117ed9b4ddd937db74399
Bubix <j23chwascik@gmail.com>
2025-04-23 22:45:48 +0200

    0.0.14
    Signed-off-by: Bubix <j23chwascik@gmail.com>

--- src/client/kotlin/team/blombix/TooltipInjector.kt
+++ src/client/kotlin/team/blombix/TooltipInjector.kt
@@ -11,10 +11,61 @@ object TooltipInjector {
     fun register() {
         ItemTooltipCallback.EVENT.register(ItemTooltipCallback { stack: ItemStack, _, _: TooltipType, lines: MutableList<Text> ->
             when (stack.item) {
-                Items.CARROT -> lines.add(Text.literal("Thirst: +3").formatted(Formatting.DARK_BLUE))
-                Items.APPLE -> lines.add(Text.literal("Thirst: +1").formatted(Formatting.DARK_BLUE))
-                Items.BEETROOT -> lines.add(Text.literal("Thirst: +2").formatted(Formatting.DARK_BLUE))
-                Items.MELON_SLICE -> lines.add(Text.literal("Thirst: +3").formatted(Formatting.DARK_BLUE))
+                Items.CARROT -> lines.add(
+                    Text.literal("Thirst: ")
+                        .formatted(Formatting.AQUA)
+                        .append(
+                            Text.literal("+3").formatted(Formatting.DARK_AQUA)
+                        )
+                )
+
+                Items.APPLE -> lines.add(
+                    Text.literal("Thirst: ")
+                        .formatted(Formatting.AQUA)
+                        .append(
+                            Text.literal("+1").formatted(Formatting.DARK_AQUA)
+                        )
+                )
+
+                Items.BEETROOT -> lines.add(
+                    Text.literal("Thirst: ")
+                        .formatted(Formatting.AQUA)
+                        .append(
+                            Text.literal("+2").formatted(Formatting.DARK_AQUA)
+                        )
+                )
+
+                Items.MELON_SLICE -> lines.add(
+                    Text.literal("Thirst: ")
+                        .formatted(Formatting.AQUA)
+                        .append(
+                            Text.literal("+3").formatted(Formatting.DARK_AQUA)
+                        )
+                )
+
+                Items.BEETROOT_SOUP -> lines.add(
+                    Text.literal("Thirst: ")
+                        .formatted(Formatting.AQUA)
+                        .append(
+                            Text.literal("+10").formatted(Formatting.DARK_AQUA)
+                        )
+                )
+
+                Items.MUSHROOM_STEW -> lines.add(
+                    Text.literal("Thirst: ")
+                        .formatted(Formatting.AQUA)
+                        .append(
+                            Text.literal("+10").formatted(Formatting.DARK_AQUA)
+                        )
+                )
+
+                Items.RABBIT_STEW -> lines.add(
+                    Text.literal("Thirst: ")
+                        .formatted(Formatting.AQUA)
+                        .append(
+                            Text.literal("+10").formatted(Formatting.DARK_AQUA)
+                        )
+                )
             }
         })
     }
