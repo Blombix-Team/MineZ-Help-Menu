801867ce2a4e20c162256047586cb5c73f462d88
Bubix <j23chwascik@gmail.com>
2025-06-12 19:53:07 +0200

    0.0.36

--- src/main/kotlin/team/blombix/MineZHelpMenu.kt
+++ src/main/kotlin/team/blombix/MineZHelpMenu.kt
@@ -22,7 +22,7 @@ object MineZHelpMenu : ModInitializer {
         logger.info("BasicAly")
         logger.info("All MineZ Wiki Editors")
         logger.info(" ")
-        logger.info("Version: 0.0.35")
+        logger.info("Version: 0.0.36")
         logger.info(" ")
         logger.info("[==============================================]")
 
